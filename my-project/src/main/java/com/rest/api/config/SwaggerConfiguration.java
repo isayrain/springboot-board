dd72fd2557e8b1cd821ab54bbbffbd4423999a8d
isayrain <pegasusjh@naver.com>
2021-08-10 23:42:25 +0900

    add swagger lib

---
+++ my-project/src/main/java/com/rest/api/config/SwaggerConfiguration.java
@@ -0,0 +1,10 @@
+package com.rest.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+}
