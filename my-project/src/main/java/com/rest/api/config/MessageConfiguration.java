71ae66c320dbe1a144690931916700c04849d676
isayrain <pegasusjh@naver.com>
2021-08-22 13:44:01 +0900

    Message gradle

--- my-project/src/main/java/com/rest/api/config/MessageConfiguration.java
+++ my-project/src/main/java/com/rest/api/config/MessageConfiguration.java
@@ -1,5 +1,6 @@
 package com.rest.api.config;
 
+import net.rakugakibox.util.YamlResourceBundle;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
